3e2204e6264566cac3ce6bc159a01c31442c15d9
nthykier <nthykier@ac69b549-5b3f-0410-8a32-0d25ecb5d1fd>
2007-11-27 15:59:56 +0000

    New Exceptions

--- Studieplan/exceptions/CourseDoesNotExistException.java
+++ Studieplan/exceptions/CourseDoesNotExistException.java
@@ -8,29 +8,15 @@ package exceptions;
  * Thrown whenever the applications attempts to look up a course that does not exist in the database.
  * This is likely due to a malformatted course number or the database is not up to date.
  */
-public class CourseDoesNotExistException extends Exception {
+public class CourseDoesNotExistException extends CourseException {
 
 	private static final long serialVersionUID = 2980651101109614877L;
-
-	/**
-	 * The course ID that did not exist
-	 */
-	private String courseDescription;
 	
 	/**
 	 * @param courseID the ID of the course that did not exist
 	 */
 	public CourseDoesNotExistException(String courseDescription) {
-		super("*** Kursus " + courseDescription + " blev ikke fundet ***");
-		this.courseDescription = courseDescription;
+		super("*** Kursus", courseDescription, "blev ikke fundet ***");
 	}
 	
-	public String getCourseDescription() {
-		return courseDescription;
-	}
-
-	
-	public String toString() {
-		return "Course " + courseDescription + " could not be found";
-	}
 }
