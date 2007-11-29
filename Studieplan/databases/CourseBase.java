bd378f5841709260baa89790368d9a4a2709f25c
nthykier <nthykier@ac69b549-5b3f-0410-8a32-0d25ecb5d1fd>
2007-11-29 14:24:03 +0000

    CourseBase fully Documented

--- Studieplan/databases/CourseBase.java
+++ Studieplan/databases/CourseBase.java
@@ -13,15 +13,34 @@ import exceptions.CourseDoesNotExistException;
 import exceptions.FilePermissionException;
 
 /**
+ * CourseBase contains all valid read from databases.
+ * It uses the DatabaseReader to read the course from the databases.
  * @author Niels Thykier
- *
  */
 public class CourseBase {
+	/**
+	 * An ArrayList containing all the courses from the databases 
+	 */
 	private ArrayList<Course> allCourses;
+	/**
+	 * DatabaseReader, used whenever it is reloading all the courses or searching in the 
+	 * databases.
+	 */
 	private DatabaseReader dbRead;
+	/**
+	 * Statistic data: amount of courses loaded during the last reload.
+	 */
 	private int amountOfCourses;
+	/**
+	 * Statistic data: time (in milliseconds) it took to load the courses doing the last reload.
+	 */
 	private long loadTime;
 	
+	/**
+	 * Constructs a new CourseBase that contains all valid courses.
+	 * @throws FileNotFoundException if the database files could not be found.
+	 * @throws FilePermissionException if the files could not be read due to missing permissions
+	 */
 	public CourseBase() throws FileNotFoundException, FilePermissionException {
 		
 		dbRead = new DatabaseReader();
@@ -30,6 +49,11 @@ public class CourseBase {
 	}
 	
 	
+	/**
+	 * Used to (re-)load all the courses from the databases.
+	 * The previous list of the Courses are voided, so only the courses in the databases
+	 * at the time of reloading will appear in the CourseBase, when it is done.
+	 */
 	public void reloadDatabase() {
 		allCourses = new ArrayList<Course>();
 		int i = 0;
@@ -48,18 +72,36 @@ public class CourseBase {
 		
 	}
 	
+	/**
+	 * Get the amount of courses loaded (statistical data) 
+	 * @return The amount of courses that was loaded during the last reload.
+	 */
 	public int getAmountOfCourses() {
 		return amountOfCourses;
 	}
 	
+	/**
+	 * Get the load average for Courses per second (statistical data)
+	 * @return The load average meassured in courses per second. 
+	 */
 	public long getLoadAverage() {
 		return (amountOfCourses*1000)/loadTime;
 	}
 	
+	/**
+	 * Get the time it took to load the courses in the last load/reload. (statistical data) 
+	 * @return the time in milliseconds it took to do the last load/reload.
+	 */
 	public long getLoadTime() {
 		return loadTime;
 	}
 	
+	/**
+	 * Look up a Course in the CourseBase by its ID.
+	 * @param courseID The ID of the course to find.
+	 * @return The Course with that given ID.
+	 * @throws CourseDoesNotExistException Thrown if the course does not exist.
+	 */
 	public Course findCourse(String courseID) throws CourseDoesNotExistException {
 		Course course;
 		Iterator<Course> ilt = allCourses.iterator();
@@ -72,14 +114,30 @@ public class CourseBase {
 		throw new CourseDoesNotExistException(courseID);
 	}
 	
+	/**
+	 * Search through the database files for a course containing a pattern.
+	 * @param pattern The pattern to search for. 
+	 * @return The Courses that match the pattern in some way.
+	 * @throws CourseDoesNotExistException
+	 * @deprecated It is slow and does not search through the courses in the CourseBase, 
+	 * but sends it on to the DatabaseReader's search.
+	 */
 	public Course[] search(String pattern) throws CourseDoesNotExistException {
 		return dbRead.search(pattern);
 	}
 	
+	/**
+	 * Get an array of all the courses in the CourseBase.
+	 * @return All the courses in the CourseBase as a Array.
+	 */
 	public Course[] getAllCourses() {
 		return allCourses.toArray(new Course[1]);
 	}
 	
+	/**
+	 * Prints a list of all the Courses in the CourseBase.
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString() {
 		
 		String toReturn = "Kursus list: \n";
